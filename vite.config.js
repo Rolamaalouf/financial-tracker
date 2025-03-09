3213ecf17a739dbfd19cd03abcf17327412307b4
Rolamaalouf <roula.maalouf@hotmail.fr>
2025-03-09 16:05:26 +0200

    for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  server: mode === 'development' ? {
+  build: {
+    outDir: 'build', // Change output directory to "build" for Vercel
+    chunkSizeWarningLimit: 800, // Increase chunk size limit to avoid warnings
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Split dependencies into a separate chunk
+          }
+        },
+      },
+    },
+  },
+  server: {
     proxy: {
       '/api': {
         target: 'https://fin-sync-1.onrender.com',
         changeOrigin: true,
         secure: false,
       },
-    }
-  } : undefined, // Disable proxy in production
-}));
+    },
+  },
+})
+
